11b165e5632ccd91dea743459bdbd089b5e2f659
Rahulbathula <146337511+Rahulbathula@users.noreply.github.com>
2023-11-02 23:59:58 +0000

    defc

--- Tasks/TASK 124-Challenge2/main.cpp
+++ Tasks/TASK 124-Challenge2/main.cpp
@@ -1,10 +1,31 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+BusOut leds(PC_7,PC_8,PC_9);
 
+int main() {
+    int pattern, count, delay;
+    
+    while (true) {
+        do {
+            printf("Enter a pattern (0-7): ");
+            scanf("%d", &pattern);
+        } while (pattern < 0 || pattern > 7);
+        
+        do {
+            printf("Enter a count (5-20): ");
+            scanf("%d", &count);
+        } while (count < 5 || count > 20);
+        
+        do {
+            printf("Enter a delay (50-2000 ms): ");
+            scanf("%d", &delay);
+        } while (delay < 50 || delay > 2000);
+        
+        for (int i = 0; i < count; i++) {
+            
+        }
     }
 }
+
+
 
